c6f224886cd0bb9c5af7bfcf9915dcee0e2c18b1
annalbenson <annalucybenson@gmail.com>
2018-04-06 15:12:08 -0500

    Initial commit

---
+++ app/src/main/java/com/annabenson/newsgateway/MainActivity.java
@@ -0,0 +1,16 @@
+package com.annabenson.newsgateway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Start Service (News Service)
+        
+    }
+}
