9df2ddd9e5c0b8ad916e4172881a5492627ec887
annalbenson <annalucybenson@gmail.com>
2018-04-06 15:25:34 -0500

    NewsReceiver Service started

--- app/src/main/java/com/annabenson/newsgateway/MainActivity.java
+++ app/src/main/java/com/annabenson/newsgateway/MainActivity.java
@@ -1,7 +1,12 @@
 package com.annabenson.newsgateway;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Start Service (News Service)
-        
+
     }
+
+
+    // Flow Charts page 2/6--> News Receiver "Class inside Main Activity"
+    public class NewsReceiver extends Service {
+
+        private static final String TAG = "NewsReceiver";
+
+
+        @Override
+        public IBinder onBind(Intent arg0){
+            Log.i(TAG, "onBind: Service onBind");
+            return null;
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId){
+            Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+        }
+
+
+        //onReceive(){
+
+        //}
+
+
+
+    }
+
+
+
 }
